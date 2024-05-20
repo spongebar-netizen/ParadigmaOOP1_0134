33e8bd827ae44afa41317d88aa71cc7bc27e0923
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-20 11:50:03 +0700

    fix typo lagi

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main() {// agregasi
+int main() {// agregasi 
 	ibu* varIbu   = new ibu("dini");
 	ibu* varIbu2  = new ibu("novi");
 	anak* varAnak1 = new anak("tono");
